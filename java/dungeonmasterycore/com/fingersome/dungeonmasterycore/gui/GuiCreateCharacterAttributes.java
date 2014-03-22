3f9bc76c084d3ed374dc76805db1c56507761408
Fingersome <teddysouth@yahoo.co.uk>
2014-03-22 11:19:18 +0000

    removed breakables, modified attributes gui and texture

--- java/dungeonmasterycore/com/fingersome/dungeonmasterycore/gui/GuiCreateCharacterAttributes.java
+++ java/dungeonmasterycore/com/fingersome/dungeonmasterycore/gui/GuiCreateCharacterAttributes.java
@@ -20,10 +20,11 @@ import com.fingersome.dungeonmasterycore.DungeonMasteryCore;
 
 public class GuiCreateCharacterAttributes extends GuiScreen 
 {
-	final int xSize = 176;
-	final int ySize = 222;
-	
 
+	ResourceLocation texture = new ResourceLocation(CoreReference.MOD_ID.toLowerCase(), "textures/gui/GuiCharacterCreationAtrributes.png");
+
+	final int xSize = 305;
+	final int ySize = 286;
 	
 	 public GuiCreateCharacterAttributes(EntityPlayer player)
 	    {
@@ -35,24 +36,24 @@ public class GuiCreateCharacterAttributes extends GuiScreen
 		int posX = (this.width - xSize) /2;
 		int posY = (this.height - ySize) /2;
 		
-		this.buttonList.add(new GuiButton(0, posX - 60, posY + 5, 20, 20, "-"));
-		this.buttonList.add(new GuiButton(1, posX - 60, posY + 25, 20, 20, "-"));
-		this.buttonList.add(new GuiButton(2, posX - 60, posY + 45, 20, 20, "-"));
-		this.buttonList.add(new GuiButton(3, posX - 60, posY + 65, 20, 20, "-"));
-		this.buttonList.add(new GuiButton(4, posX - 60, posY + 85, 20, 20, "-"));
-		this.buttonList.add(new GuiButton(5, posX - 60, posY + 105, 20, 20, "-"));
-		this.buttonList.add(new GuiButton(0, posX - 30, posY + 5, 20, 20, "+"));
-		this.buttonList.add(new GuiButton(1, posX - 30, posY + 25, 20, 20, "+"));
-		this.buttonList.add(new GuiButton(2, posX - 30, posY + 45, 20, 20, "+"));
-		this.buttonList.add(new GuiButton(3, posX - 30, posY + 65, 20, 20, "+"));
-		this.buttonList.add(new GuiButton(4, posX - 30, posY + 85, 20, 20, "+"));
-		this.buttonList.add(new GuiButton(5, posX - 30, posY + 105, 20, 20, "+"));
+		this.buttonList.add(new GuiButton(0, posX + 10, posY + 5, 20, 20, "-"));
+		this.buttonList.add(new GuiButton(1, posX + 10, posY + 25, 20, 20, "-"));
+		this.buttonList.add(new GuiButton(2, posX + 10, posY + 45, 20, 20, "-"));
+		this.buttonList.add(new GuiButton(3, posX + 10, posY + 65, 20, 20, "-"));
+		this.buttonList.add(new GuiButton(4, posX + 10, posY + 85, 20, 20, "-"));
+		this.buttonList.add(new GuiButton(5, posX + 10, posY + 105, 20, 20, "-"));
+		this.buttonList.add(new GuiButton(0, posX + 40, posY + 5, 20, 20, "+"));
+		this.buttonList.add(new GuiButton(1, posX + 40, posY + 25, 20, 20, "+"));
+		this.buttonList.add(new GuiButton(2, posX + 40, posY + 45, 20, 20, "+"));
+		this.buttonList.add(new GuiButton(3, posX + 40, posY + 65, 20, 20, "+"));
+		this.buttonList.add(new GuiButton(4, posX + 40, posY + 85, 20, 20, "+"));
+		this.buttonList.add(new GuiButton(5, posX + 40, posY + 105, 20, 20, "+"));
 
-		this.buttonList.add(new GuiButton(8, posX - 60, posY + 145, 100, 20, "Random"));
-		this.buttonList.add(new GuiButton(8, posX + 60, posY + 145, 100, 20, "Reset"));	
+		this.buttonList.add(new GuiButton(8, posX + 10, posY + 145, 100, 20, "Random"));
+		this.buttonList.add(new GuiButton(8, posX + 10, posY + 175, 100, 20, "Reset"));	
 		
-		this.buttonList.add(new GuiButton(17, posX + 160, posY + 215, 100, 20, "Next"));
-		this.buttonList.add(new GuiButton(18, posX - 60, posY + 215, 100, 20, "Back"));
+		this.buttonList.add(new GuiButton(17, posX + 10, posY + 245, 100, 20, "Next"));
+		this.buttonList.add(new GuiButton(18, posX + 195, posY + 245, 100, 20, "Back"));
 		
 		}
 
@@ -94,14 +95,20 @@ public class GuiCreateCharacterAttributes extends GuiScreen
         drawDefaultBackground();
         //drawGradientRect(20, 20, width - 20, height - 20, 0x60500000, 0xa0803030);
         //drawRect(60, 60, width - 60, height - 60, 0xFF9E9E9E);
-        //drawTexturedModalRect();
-        drawCenteredString(fontRendererObj, "Choose your Attributes", width / 2, 		height / 2 - 150, 0xffffff);
-        drawString(fontRendererObj, "Strength", 				width / 2 - 80, 	height / 2 - 100, 0xffffff);
-        drawString(fontRendererObj, "Dexterity", 				width / 2 - 80, 	height / 2 - 80, 0xffffff);
-        drawString(fontRendererObj, "Constitution", 			width / 2 - 80, 	height / 2 - 60, 0xffffff);
-        drawString(fontRendererObj, "Intellect", 				width / 2 - 80, 	height / 2 - 40, 0xffffff);
-        drawString(fontRendererObj, "Wisdom", 					width / 2 - 80, 	height / 2 - 20, 0xffffff);
-        drawString(fontRendererObj, "Charisma", 				width / 2 - 80,	height / 2 - 0, 0xffffff);
+        
+        int posX = (this.width - xSize) / 2;
+        int posY = (this.height - ySize) / 2;
+        
+        //Minecraft.getMinecraft().getTextureManager().bindTexture(texture);     
+        //drawTexturedModalRect(posX, posY, 0, 0, xSize, ySize);
+        
+        drawCenteredString(fontRendererObj, "Choose your Attributes", width / 2, 		height / 2 - 160, 0xffffff);
+        drawString(fontRendererObj, "Strength", 				posX + 80, 		posY + 10, 0xffffff);
+        drawString(fontRendererObj, "Dexterity", 				posX + 80, 		posY + 30, 0xffffff);
+        drawString(fontRendererObj, "Constitution", 			posX + 80, 		posY + 50, 0xffffff);
+        drawString(fontRendererObj, "Intellect", 				posX + 80, 		posY + 70, 0xffffff);
+        drawString(fontRendererObj, "Wisdom", 					posX + 80, 		posY + 90, 0xffffff);
+        drawString(fontRendererObj, "Charisma", 				posX + 80,		posY + 110, 0xffffff);
       
         super.drawScreen(i, j, f);
 	}
